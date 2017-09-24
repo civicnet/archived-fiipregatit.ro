02369417d67f2a5a71ca94af1f3a0c442fa77078
amsterdammer <radus.playground@gmail.com>
2017-09-24 23:32:28 +0200

    FPRG-8: extra entities + details page

---
+++ site/web/app/lib/Entity/LinkUtil.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Entity;
+
+
+class LinkUtil extends Entity
+{
+    /**
+     * @var
+     */
+    private $_title;
+    /**
+     * @var
+     */
+    private $_target;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->_title;
+    }
+
+    /**
+     * @param mixed $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->_title = $title;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTarget()
+    {
+        return $this->_target;
+    }
+
+    /**
+     * @param mixed $target
+     * @return $this
+     */
+    public function setTarget($target)
+    {
+        $this->_target = $target;
+        return $this;
+    }
+
+
+}
--- site/web/app/themes/sage/home.php
+++ site/web/app/themes/sage/home.php
@@ -4,8 +4,15 @@
 <?php
 
 $ghiduri = \RepoManager::getGhidurEducativeRepository()->getList(App::HOMEPAGE_GHID_COUNT);
-
 var_dump($ghiduri);
+
+$linkuriUtile = \RepoManager::getLinkuriUtileRepository()->getList(App::LINKURI_UTILE_COUNT);
+var_dump($linkuriUtile);
+
+$campanii = \RepoManager::getCampaniiRepository()->getList(App::HOMEPAGE_CAMPANII_COUNT);
+var_dump($campanii);
+
+
 
 ?>
 

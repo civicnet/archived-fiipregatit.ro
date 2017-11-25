6e731fecaf87a0964ea047437fa0b557d20d8e79
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2017-11-25 23:44:11 +0200

    Add homepage skeleton
     - added ugly homepage skeleton
     - added updated wp export file
    
    This commit addresses #13

--- site/web/app/themes/sage/base.php
+++ site/web/app/themes/sage/base.php
@@ -18,7 +18,7 @@ use Roots\Sage\Wrapper;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container" role="document">
+    <div class="wrap container-fluid" role="document">
       <div class="content row">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -3,5 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Merriweather|Roboto" rel="stylesheet">
+  <script src="https://use.fontawesome.com/26b5b263d0.js"></script>
   <?php wp_head(); ?>
 </head>

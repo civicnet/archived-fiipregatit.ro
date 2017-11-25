b1168b12ec3d87106eeeb0d563f64c02ccba5679
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2017-11-25 02:09:22 +0200

    Add basic header nav
    - Added basic header nav
    - Updated export file with all the pages
    - Added `.DS_Store` files to `.gitignore`
    
    Fixes #14

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather|Roboto" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
--- site/web/app/themes/sage/templates/header.php
+++ site/web/app/themes/sage/templates/header.php
@@ -3,9 +3,9 @@
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav class="nav-primary">
       <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
+        if (has_nav_menu('primary_navigation')) {
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        }
       ?>
     </nav>
   </div>

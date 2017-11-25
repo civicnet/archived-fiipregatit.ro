65bdbad652dce4e4e03a2c3649c8ce8136fbae65
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2017-11-26 01:17:35 +0200

    Update homepage header section to match design
    - Added background image
    - Fancy header text
    - Edited search box style
    
    Note: Google version of the Merriweather font is missing Romanian
    glyphs, had to use Typekit instead

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Merriweather|Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.typekit.net/sac3zmn.css">
   <script src="https://use.fontawesome.com/26b5b263d0.js"></script>
   <?php wp_head(); ?>
 </head>

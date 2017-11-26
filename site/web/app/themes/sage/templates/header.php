a62cd213458549ee317bd8d325f1821b70c13383
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2017-11-26 05:46:06 +0200

    Better header

--- site/web/app/themes/sage/templates/header.php
+++ site/web/app/themes/sage/templates/header.php
@@ -1,12 +1,41 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-        if (has_nav_menu('primary_navigation')) {
-          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-        }
-      ?>
-    </nav>
-  </div>
-</header>
+<?php
+	use Roots\Sage\Assets;
+?>
+
+<nav class="navbar navbar-toggleable-md navbar-light fixed-top">
+  <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+    <img
+      src="<?=Assets\asset_path('images/logo.png');?>"
+      height="60"
+      class="d-inline-block align-top"
+      alt="">
+    <div>
+      <strong>fiipregatit</strong>.ro
+      <span class="subtitle">
+        Departamentul pentru Situații de Urgență
+      </span>
+    </div>
+  </a>
+
+  <button
+		class="navbar-toggler navbar-toggler-right"
+		type="button"
+		data-toggle="collapse"
+		data-target="#navbarContent"
+		aria-controls="navbarContent"
+		aria-expanded="false"
+		aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+  </button>
+
+  <?php
+    if (has_nav_menu('primary_navigation')) {
+      wp_nav_menu([
+        'theme_location' => 'primary_navigation',
+        'menu_class' => 'navbar-nav',
+        'container_class' => 'collapse navbar-collapse justify-content-end',
+        'container_id' => 'navbarContent'
+      ]);
+    }
+  ?>
+</nav>

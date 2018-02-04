06a1f5437b13cb70207664854ae26d3cc6d9b97c
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-04 22:13:26 +0200

    feat(homepage) Homepage complete UI
    Fixed all of the outstanding issues. There are still some issues but
    it's in a much better shape than before.

--- site/web/app/themes/sage/home.php
+++ site/web/app/themes/sage/home.php
@@ -8,30 +8,32 @@
 ?>
 
   <div class="container-fluid">
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1>Fii pregătit</h1>
-        <h3>pentru</h3>
-        <h1 class="subtitle">situații de urgență</h1>
-        <div class="align-self-center">
-          <div class="input-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Scrie aici, de ex: furtuna"
-              aria-label="Search for...">
-            <span class="input-group-btn">
-              <button class="btn btn-secondary" type="button">
-                <i class="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </span>
+    <div class="row">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1>Fii pregătit</h1>
+          <h3>pentru</h3>
+          <h1 class="subtitle">situații de urgență</h1>
+          <div class="align-self-center">
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Scrie aici, de ex: furtună"
+                aria-label="Scrie aici, de ex: furtună">
+              <span class="input-group-btn">
+                <button class="btn btn-secondary" type="button">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </span>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="container-fluid ghiduri-section">
+  <div class="container-fluid" id="ghiduri-section">
     <div class="container">
       <h2>Ghiduri educative</h2>
       <div class="row ghid-row">
@@ -45,17 +47,15 @@
 ?>
     <div class="col ghid-box col-lg-3 col-md-3 col-sm-3 col-xs-6">
       <div class="ghid-content">
+        <div
+          style="background-image: url(<?=$ghid->getPictograma()->getUrl(); ?>)"
+          class="guide-icon">
+        </div>
         <h3>
           <?=$ghid->getTitle();?>
         </h3>
-        <img
-          src="<?=$ghid->getPictograma()->getUrl(); ?>"
-          class="d-block mx-auto mt-3 mb-3 rounded-circle border border-dark"
-          width="80"
-          height="80"
-        />
         <a
-          class="btn btn-outline-secondary rounded-0"
+          class="outline-button btn btn-outline-secondary rounded-0"
           role="button"
           href="<?=$ghid->getPermalink(); ?>">
           Vezi Ghid
@@ -74,65 +74,65 @@
 ?>
       </div>
       <div class="text-center">
-        <button type="button" class="btn btn-secondary all-button">
+        <a href="#" rel="button" class="btn btn-secondary all-button">
           Vezi toate ghidurile
           <i class="fa fa-chevron-right" aria-hidden="true"></i>
-        </button>
+        </a>
       </div>
     </div>
   </div>
 
-  <div class="container-fluid summary-section">
+  <div class="container-fluid" id="summary-section">
     <div class="container">
       <div class="row">
         <div class="col">
-          <i class="fa fa-file-text-o" aria-hidden="true"></i>
+          <i class="far fa-file-alt" data-fa-transform="grow-25"></i>
           <div class="count">
             <span>76</span> ghiduri educative
           </div>
         </div>
         <div class="col">
-          <i class="fa fa-camera" aria-hidden="true"></i>
+          <i class="fa fa-camera" data-fa-transform="grow-25"></i>
           <div class="count">
-            <span>384</span> imagini si ilustratii
+            <span>384</span> imagini si ilustrații
           </div>
         </div>
         <div class="col">
-          <i class="fa fa-film" aria-hidden="true"></i>
+          <i class="fa fa-film" data-fa-transform="grow-25"></i>
           <div class="count">
-            <span>25</span> video instructiuni
+            <span>25</span> video instrucțiuni
           </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="container-fluid plan-personal-section">
+  <div class="container-fluid" id="plan-personal-section">
     <div class="container">
       <h2>Plan personal</h2>
       <div class="row">
-        <div class="col">
+        <div class="col d-none d-sm-block img-container">
           <img
             src="<?=Assets\asset_path('images/plan_personal_homepage.jpg');?>"
             class="img-fluid">
         </div>
         <div class="col">
           <div class="headline">
-            Intr-o situatie de urgenta reactionezi corect daca esti
-            <span class="big-red">informat, pregatit, solidar</span>.
+            Într-o situație de urgență reacționezi corect dacă ești
+            <span class="big-red">informat, pregătit, solidar</span>.
           </div>
           <div>
-            Planul personal pentru situatii de urgenta este compus in principal
-            din 3 elemente de baza:
+            Planul personal pentru situații de urgență este compus în principal
+            din 3 elemente de bază:
           </div>
           <div class="element">
-            Sa fii informat si pregatit inainte de situatie
+            Să fii informat și pregătit înainte de situație
           </div>
           <div class="element">
-            Sa stii ce trebuie sa faci in timpul unei situatii
+            Să știi ce trebuie să faci în timpul unei situații
           </div>
           <div class="element">
-            Sa ai pregatit rucsacul cu cele necesare supravietuirii
+            Să ai pregătit rucsacul cu cele necesare supraviețuirii
           </div>
         </div>
       </div>
@@ -157,7 +157,7 @@
   foreach ($campaigns as $campaign) {
     $count_campaigns++;
 ?>
-    <div class="col campaign-box col ghid-box col-lg-3 col-md-3 col-sm-3 col-xs-6">
+    <div class="col campaign-box col-lg-4 col-md-6 col-sm-12 col-xs-12">
       <div class="campaign-content">
         <h3>
           <?=$campaign->getTitle();?>
@@ -166,19 +166,18 @@
           <i class="far fa-calendar"></i>
           <?=$campaign->getDate()->format('d.m.Y');?>
         </div>
-        <img
-          src="<?=$campaign->getImage()->getUrl(); ?>"
-          class="d-block mx-auto mt-3 mb-3 border border-dark"
-          width="225"
-        />
+        <div
+          class="d-block mx-auto mt-3 mb-3 border campaign-photo"
+          style="background-image: url(<?=$campaign->getImage()->getUrl(); ?>)">
+        </div>
         <div class="campaign-extras">
             <?=$campaign->getExtras();?>
         </div>
         <a
           href="<?=$campaign->getPermalink();?>"
-          rel="button"
-          class="btn btn-outline-secondary rounded-0">
-          Citeste
+          role="button"
+          class="outline-button btn btn-outline-secondary rounded-0">
+          Citește
           <i class="fa fa-chevron-right" aria-hidden="true"></i>
         </a>
       </div>
@@ -203,19 +202,34 @@
     </div>
   </div>
 
-  <div class="container-fluid app-section">
+  <div class="container-fluid" id="app-section">
     <div class="container">
       <div class="row">
-        <div class="col">
+        <div class="col d-none d-sm-block ">
           <img
             src="<?=Assets\asset_path('images/app_mobil_dsu.jpg');?>"
             class="img-fluid">
         </div>
         <div class="col">
           <h2>
-            Afla primul despre alerte din trafic, avertizari meteo
-            sau calamitati naturale
+            Află primul despre alerte din trafic, avertizări meteo
+            sau calamități naturale
           </h2>
+          <div class="app-intro">
+            <div class="subtitle">
+              Descarcă <strong>gratuit</strong> aplicația DSU!
+            </div>
+            <div class="row">
+              <div class="col">
+                <img src="<?=Assets\asset_path('images/ios-badge.svg');?>" />
+              </div>
+              <div class="col">
+                <img
+                  class="play-store"
+                  src="<?=Assets\asset_path('images/playstore-badge.svg');?>" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -1,5 +1,114 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+<?php
+  /***
+   * Template Name: Custom Homepage
+   **/
+
+  use Roots\Sage\Assets;
+?>
+
+<footer>
+  <div class="container content-info">
+    <div class="row">
+      <div class="col page-list col-lg-2 col-md-2 col-sm-12 col-xs-12">
+        <ul>
+        <?php
+          $pages = get_pages();
+          $is_first = true;
+          foreach ($pages as $page) {
+        ?>
+          <li>
+            <a href="<?=$post->guid;?>" <?=$is_first ? 'class="header"' : ''?>>
+              <?=$page->post_title;?>
+            </a>
+          </li>
+        <?php
+            if ($is_first) {
+              $is_first = false;
+            }
+          }
+        ?>
+        </ul>
+      </div>
+
+      <div class="col link-list col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="header">
+          Legături utile
+        </div>
+        <ul>
+        <?php
+          $links = \RepoManager::getLinkuriUtileRepository()
+            ->getList(App::LINKURI_UTILE_COUNT);
+          foreach($links as $link) {
+        ?>
+          <li>
+            <a href="<?=$link->getTarget();?>">
+              <?=$link->getTitle();?>
+            </a>
+          </li>
+        <?php
+          }
+        ?>
+        </ul>
+      </div>
+
+      <div class="col contact col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class="header">
+          Contact
+        </div>
+        <div>
+          Adresă: Piața Revoluției, nr.1 A, sector 1,<br/>
+          București<br/>
+          Telefon <strong>021.312.25.47</strong><br/>
+          Centrala 021.303.70.80<br/><br/>
+
+          Fax: 021.313.71.55/021.264.86.46<br/>
+          E-mail: <strong>dsu@mai.gov.ro</strong><br/>
+        </div>
+      </div>
+
+      <div class="col social-media col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="header">
+          Social Media
+        </div>
+        <ul>
+          <li>
+            <i class="fab fa-facebook-square" data-fa-transform="grow-10"></i>
+            Facebook
+          </li>
+          <li>
+            <i class="fab fa-twitter-square" data-fa-transform="grow-10"></i>
+            Twitter
+          </li>
+          <li>
+            <i class="fab fa-youtube-square" data-fa-transform="grow-10"></i>
+            YouTube
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="bottom container-fluid">
+    <div class="row">
+      <div class="col col-lg-2 col-md-2 d-none d-sm-block "></div>
+      <div class="col col-md-3 col-sm-12 col-xs-12">
+        <ul>
+          <li class="gov">
+            <img src="<?=Assets\asset_path('images/logo_guvern.png');?>" />
+          </li>
+          <li class="dsu">
+            <img src="<?=Assets\asset_path('images/logo.png');?>" />
+          </li>
+        </ul>
+      </div>
+      <div class="col col-lg-3 col-md-5 col-sm-12 col-xs-12">
+        <div class="copyright">
+          <h4>Departamentul pentru Situații de Urgență</h4>
+          <div class="sub">
+            Realizat cu sprijinul <a href="https://civictech.ro">CivicTech</a>. Toate drepturile rezervate.<br/>
+            Copyright @  2017. Toate drepturile rezervate.
+          </div>
+        </div>
+      </div>
+      <div class="col col-lg-2 col-md-2 d-none d-sm-block "></div>
   </div>
 </footer>

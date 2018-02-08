bb6f5e33a3c50d74c82a925ceba90a102ba5d1f0
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-08 14:11:38 +0200

    Add Algolia search

--- site/web/app/lib/Repository/CampaignRepository.php
+++ site/web/app/lib/Repository/CampaignRepository.php
@@ -11,14 +11,14 @@ final class CampaignRepository extends AbstractRepository {
     bool $includeRelated = false
   ): Campaign {
     return (new Campaign($post->ID))
-      ->setTitle($post->post_title)
-      ->setContent(get_field('continut'))
+      ->setTitle($post->post_title, $post->ID)
+      ->setContent(get_field('continut', $post->ID))
       ->setDate(new \DateTime($post->post_date))
-      ->setPermalink(get_the_permalink())
-      ->setImage(get_field('imagine'))
-      ->setExtras(get_field('extras'))
+      ->setPermalink(get_the_permalink($post->ID))
+      ->setImage(get_field('imagine', $post->ID))
+      ->setExtras(get_field('extras', $post->ID))
       ->setAttachments(static::falseyToNull(
-        get_field('materiale_de_informare')
+        get_field('materiale_de_informare', $post->ID)
       ));
   }
 }

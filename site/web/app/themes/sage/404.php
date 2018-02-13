4b82705c10340b2757ae1eb8077063996ad3f8aa
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-13 17:56:45 +0200

    fix(404) Translate error message

--- site/web/app/themes/sage/404.php
+++ site/web/app/themes/sage/404.php
@@ -1,7 +1,16 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
+  get_template_part('templates/page', 'header');
+
+  TemplateEngine::get()->render(
+    'jumbotron',
+    array(
+      'show_header' => false,
+      'extra_class' => 'small-jumbotron',
+      'algolia_search' => get_search_form($echo = false)
+    )
+  );
+?>
 
 <div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+  <?php _e('Ne cerem scuze, dar pagina pe care ați încercat să o accesați nu este disponibilă.', 'sage'); ?>
 </div>
-
-<?php get_search_form(); ?>

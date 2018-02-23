2cab1e6bd993bb3a10de0c033493b20c01aae170
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-23 05:20:56 +0200

    chore(analytics) Add Google Analytics tracking code

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -155,3 +155,13 @@
       });
   });
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114659863-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114659863-1');
+</script>

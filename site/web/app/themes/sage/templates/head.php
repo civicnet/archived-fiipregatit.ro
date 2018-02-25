8bd172d7956a4b5b2d54a1d02ce163849806433c
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-25 12:33:18 +0200

    fix(favicon) Add favicon

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -1,7 +1,11 @@
+<?php
+  use Roots\Sage\Assets;
+?>
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="<?= Assets\asset_path('images/Logo_DSU.ico') ?>" />
   <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700|Roboto:300,400,700" rel="stylesheet">
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   <?php wp_head(); ?>

4fe36532ac08db1ecf9d18d1e293d7fa6f9146c7
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-26 14:46:03 +0200

    fix(favicon) Change favicon

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="<?= Assets\asset_path('images/Logo_DSU.ico') ?>" />
+  <link rel="icon" type="image/png" href="<?= Assets\asset_path('images/fiipregatit_favicon_32x32.png') ?>" />
   <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700|Roboto:300,400,700" rel="stylesheet">
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   <?php wp_head(); ?>

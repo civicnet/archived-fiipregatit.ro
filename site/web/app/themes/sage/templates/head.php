551f90eb7e01d32605235894742f9e084e859e26
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-28 04:31:27 +0200

    fix(cookie) Cookie consent popup

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -9,4 +9,27 @@
   <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700|Roboto:300,400,700" rel="stylesheet">
   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   <?php wp_head(); ?>
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+  <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+  <script>
+  window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#ffffcc",
+          "text": "#993333"
+        },
+        "button": {
+          "background": "#993333"
+        }
+      },
+      "showLink": false,
+      "position": "bottom-right",
+      "content": {
+        "message": "Pentru o experiență cât mai bună, acest website folosește cookies.",
+        "dismiss": "Am înțeles",
+        "link": ""
+      }
+    })});
+  </script>
 </head>

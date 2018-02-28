01b5d62e8dd84e4189c0cdbdc9d6c3a75236b5d3
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-03-01 01:49:14 +0200

    fix(footer) proper links in footer

--- site/web/app/lib/CustomPageManager.php
+++ site/web/app/lib/CustomPageManager.php
@@ -1,40 +1,40 @@
 <?php
 
-  use CustomPage\PersonalPlanPage;
-  use CustomPage\FirstAidPage;
-  use CustomPage\AboutPage;
+use CustomPage\PersonalPlanPage;
+use CustomPage\FirstAidPage;
+use CustomPage\AboutPage;
 
-  final class CustomPageManager {
-      private function __construct() {}
+final class CustomPageManager {
+  private function __construct() {}
 
-      public static function getPersonalPlanPage(): PersonalPlanPage {
-        return self::getPage(App::PAGE_PERSONAL_PLAN);
-      }
-
-      public static function getFirstAidPage(): FirstAidPage {
-        return self::getPage(App::PAGE_FIRST_AID);
-      }
-
-      public static function getAboutPage(): AboutPage {
-        return self::getPage(App::PAGE_ABOUT);
-      }
-
-
-      private static function getPage(
-        string $key
-      )/*: AbstractRepository*/ {
-        switch ($key) {
-          case App::PAGE_PERSONAL_PLAN:
-            return PersonalPlanPage::get();
-          case App::PAGE_FIRST_AID:
-            return FirstAidPage::get();
-          case App::PAGE_ABOUT:
-            return AboutPage::get();
-        }
-
-        throw new Exception(sprintf(
-          'Invalid repository type `%s`',
-          $repositoryKey
-        ));
-      }
+  public static function getPersonalPlanPage(): PersonalPlanPage {
+    return self::getPage(App::PAGE_PERSONAL_PLAN);
   }
+
+  public static function getFirstAidPage(): FirstAidPage {
+    return self::getPage(App::PAGE_FIRST_AID);
+  }
+
+  public static function getAboutPage(): AboutPage {
+    return self::getPage(App::PAGE_ABOUT);
+  }
+
+
+  private static function getPage(
+    string $key
+  )/*: AbstractRepository*/ {
+    switch ($key) {
+      case App::PAGE_PERSONAL_PLAN:
+        return PersonalPlanPage::get();
+      case App::PAGE_FIRST_AID:
+        return FirstAidPage::get();
+      case App::PAGE_ABOUT:
+        return AboutPage::get();
+    }
+
+    throw new Exception(sprintf(
+      'Invalid repository type `%s`',
+      $repositoryKey
+    ));
+  }
+}

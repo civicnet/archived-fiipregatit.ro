10bd3870e15641088eecf236a59284fac6089065
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-28 19:36:12 +0200

    fix(meta) Add meta tags generator (meta-meta)

--- site/web/app/themes/sage/lib/Meta/constants/Meta/MetaMetaCategory.php
+++ site/web/app/themes/sage/lib/Meta/constants/Meta/MetaMetaCategory.php
@@ -7,4 +7,19 @@
     const DESCRIPTION = 'description';
     const RATING = 'rating';
     const WEB_AUTHOR = 'web_author';
+    const GOOGLE_VERIFICATION = 'google-site-verification';
+    const COPYRIGHT = 'copyright';
+    const AUTHOR = 'author';
+
+    public static function getList(): array {
+      return array(
+        self::TITLE,
+        self::DESCRIPTION,
+        self::RATING,
+        self::WEB_AUTHOR,
+        self::GOOGLE_VERIFICATION,
+        self::COPYRIGHT,
+        self::AUTHOR,
+      );
+    }
   }

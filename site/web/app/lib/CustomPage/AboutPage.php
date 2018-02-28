73d067602a8e9a6cad27e91b849e22eba9a66f93
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2018-02-28 04:00:09 +0200

    feat(about) Add partners page layout

--- site/web/app/lib/CustomPage/AboutPage.php
+++ site/web/app/lib/CustomPage/AboutPage.php
@@ -10,8 +10,19 @@
       return (new About($this->wpObject->ID))
         ->setDespre(get_field('despre', $this->wpObject->ID))
         ->setContext(get_field('context', $this->wpObject->ID))
-        ->setParteneri(get_field('parteneri', $this->wpObject->ID))
         ->setEchipa(get_field('echipa', $this->wpObject->ID))
-        ->setPermalink(get_page_link($this->wpObject->ID));
+        ->setPermalink(get_page_link($this->wpObject->ID))
+        ->setParteneri(array_filter(
+          (array) get_post_meta(
+            $this->wpObject->ID,
+            \App::ABOUT_PAGE_METABOX_PARTNERS,
+            true
+          )
+        ))
+        ->setDescriereParteneri(get_post_meta(
+          $this->wpObject->ID,
+          \App::ABOUT_PAGE_METABOX_PARTNER_DESC,
+          true
+        )); 
     }
   }
